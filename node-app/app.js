cf2b90e9358d463b16b9d7e272d5a684a11ba953
Sravanthi Personal <chilkamollasravanthi999@email.com>
2026-03-15 20:51:48 +0530

    added jenkins path

--- node-app/app.js
+++ node-app/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
-    res.send("Node Microservice running in EKS ThankYou")
+    res.send("Node Microservice running in EKS")
 })
 
 app.listen(3000,()=>{
