889f63736cca7ec4df8077ae6b937f03c4b4b46c
Sravanthi Personal <chilkamollasravanthi999@email.com>
2026-03-15 20:46:39 +0530

    trigger jenkins app.js

--- node-app/app.js
+++ node-app/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
-    res.send("Node Microservice running in EKS")
+    res.send("Node Microservice running in EKS ThankYou")
 })
 
 app.listen(3000,()=>{
